f9b34947355ba1484509c16fdcf9aed3b7fbe4e6
Lavanyamadli <lavanyamadli2003@gmail.com>
2024-10-27 09:47:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+var username="gmit";
+var password="gmit123";
+
+function fnValidate(){
+    let inputUsername=document.getElementById("Username").value;
+    let inputpassword=document.getElementById("Password").value;
+}
+
+if(true)
+
+{
+    console.log("success");
+}else{
+    console.log("failure");
+}
+
+function fnValidateSignupForm(){
+    //let firstName=document.getElementById("firstName")[0].value;
+    let fname=document.getElementById("lastName").value;
+    let lname=document.getElementById("lastName").value;
+    let email=document.getElementById("email").value;
+    let Password=document.getElementById("password").value;
+    let ConformPassword=document.getElementById("conform").value;
+   // console.loog(firstName);
+    //console.log("___________________");
+    //console.log(fname);
+let validateFname=document.getElementById("validateFname").value;
+}
+function fnAdd(){
+    console.log(2,3)
+}
